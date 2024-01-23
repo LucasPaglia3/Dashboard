ceddc2f2a2fc0b2751628c61a37483758cab9f6a
lucaspaglia3 <lucaspaglia32@gmail.com>
2024-01-23 03:13:15 -0300

    Boton para guardar pdf en la tabla de horarios

--- src/features/Empleados/Columns.jsx
+++ src/features/Empleados/Columns.jsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import { Download } from "lucide-react";
+
 export const columns = [
   {
     accessorKey: "mes",
@@ -19,5 +22,23 @@ export const columns = [
       return value.includes(row.getValue(año));
     },
     header: "Año",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const historial = row.original;
+      return (
+        <div className="flex justify-center">
+          {" "}
+          <Button
+            className=" border-slate-300 shadow-sm"
+            variant="outline"
+            onClick={() => console.log(historial.id)}
+          >
+            <Download className="h-5 w-5 mr-2" /> <span>Descargar PDF</span>
+          </Button>
+        </div>
+      );
+    },
   },
 ];
