be2d880be349c8789d9d5bb8a1e021c7887585ca
lucaspaglia3 <lucaspaglia32@gmail.com>
2024-02-10 17:15:43 -0300

    más fixes para mobile, falta fixear página trabajo, no está cambiando la cantidad de cols

--- src/features/Trabajos/CardImagenTrabajo.jsx
+++ src/features/Trabajos/CardImagenTrabajo.jsx
@@ -2,7 +2,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const CardImagenTrabajo = ({ trabajo }) => {
   return (
-    <Card className="shadow-md col-span-1 lg:col-span-2 h-[32rem]">
+    <Card className="shadow-md lg:col-span-2 h-[32rem]">
       <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
         <CardTitle className="text-md font-medium">Imagen</CardTitle>
       </CardHeader>
